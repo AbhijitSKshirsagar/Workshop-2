dc841295655c5ec114ba3c4af9ff33da26d4eb02
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-03-09 22:43:00 +0530

    added: number players plyaing game

--- src/com/bridgelabz/DeckofCards.java
+++ src/com/bridgelabz/DeckofCards.java
@@ -1,19 +1,21 @@
 /*
- * Write a Program to initialise deck of cards having suit ("Clubs", "Diamonds", "Hearts", "Spades") & Rank ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"). Shuffle the cards using Random method and then distribute 9 Cards to 4 Players and Print the Cards the received by the 4 Players.
+  Write a Program to initialise deck of cards having suit ("Clubs", "Diamonds", "Hearts", "Spades") & Rank ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"). Shuffle the cards using Random method and then distribute 9 Cards to 4 Players and Print the Cards the received by the 4 Players.
 
 1. Ability to work with GIT
 2. Expected to have OOPs
 3. Expected to have Data Structures
 
-UC1 : Setup the initial game with deck of cards and make sure we have unique cards, 
-The number of cards are 52. Should be able to make unique deck. Maintain the ranks to further compare
+ Moving forward we need to have more than 2 players and Max 4 Players, Create Players Class to add Players from console.
+
  */
 
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DeckofCards {
+	Scanner scanner = new Scanner(System.in);
 	public ArrayList<String> cardsDeck = new ArrayList<String>();
 
 	public void deckofCards() {
@@ -38,11 +40,22 @@ public class DeckofCards {
 		System.out.println();
 	}
 
+	// Created a method for how many players will play the game
+	public void noOfPlayers() {
+		System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
+		int player = scanner.nextInt();
+		if (player >= 2 && player <= 4) {
+			System.out.println("\n" + player + " players will play the game");
+		} else {
+			System.out.println("Please enter number of players in the Range");
+		}
+		scanner.close();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Deck of Cards the Game");
-		// calling to deck of cards class
 		DeckofCards deckofCards = new DeckofCards();
 		deckofCards.deckofCards();
-
+		deckofCards.noOfPlayers();
 	}
 }
